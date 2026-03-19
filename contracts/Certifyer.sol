2be09a43b6c97aff89a72962c207b5d154e2a448
4nddrs <joseandres190503@gmail.com>
2026-03-19 21:35:52 +0000

    Update Certifyer

--- contracts/Certifyer.sol
+++ contracts/Certifyer.sol
@@ -1,28 +1,28 @@
 // // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract Certificador {
+contract Certifyer {
   address public admin;
 
   //Mapping to save: Documents and their hash => true is valid
-  mapping(bytes32 => bool) public certificados;
+  mapping(bytes32 => bool) public certificates;
 
   //Event to say to the world that a document has been certified
-  event CertificadoCreado(bytes32 indexed datahash, uint256 timestamp);
+  event CertificateCreated(bytes32 indexed datahash, uint256 timestamp);
 
   constructor(){
     admin = msg.sender;
   }
 
   //Just the admin can certify documents
-  function registrarCertificado(bytes32 datahash) public {
+  function registerCertificate(bytes32 datahash) public {
     require(msg.sender == admin, "Only the admin can certify documents");
-    certificados[datahash] = true;
-    emit CertificadoCreado(datahash, block.timestamp);
+    certificates[datahash] = true;
+    emit CertificateCreated(datahash, block.timestamp);
   }
 
   //Anyone can check if a document is certified by its hash
-  function validarCertificado(bytes32 datahash) public view returns (bool) {
-    return certificados[datahash];
+  function validateCertificate(bytes32 datahash) public view returns (bool) {
+    return certificates[datahash];
   }
 }
